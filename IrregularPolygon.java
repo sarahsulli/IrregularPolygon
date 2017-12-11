05887a50326f1423506f4e010f6e186d438663bb
Nick Hernan <nh9838@pleasantonusd.net>
2017-12-11 09:32:56 -0800

    need to dowload draw thing on mac

---
+++ IrregularPolygon.java
@@ -0,0 +1,34 @@
+import java.awt.geom.*;     // for Point2D.Double
+import java.util.ArrayList; // for ArrayList
+import gpdraw.*;            // for DrawingTool
+
+public class IrregularPolygon{
+   private ArrayList <Point2D.Double> myPolygon;
+
+   // constructors
+   public IrregularPolygon() 
+   {
+    
+   }
+
+   // public methods
+   public void add(Point2D.Double aPoint) 
+   { 
+    
+   }
+
+   public void draw() 
+   { 
+    
+   }
+
+   public double perimeter()
+   { 
+       
+   }
+
+   public double area() 
+   {
+    
+   }
+}
