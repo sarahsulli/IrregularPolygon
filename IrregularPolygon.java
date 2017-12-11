b4eead67eb6488c614f84d6b7757ebbd6330dff1
Sarah Sullivan <sullytwin246@gmail.com>
2017-12-11 15:56:40 -0800

    wowza

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -2,7 +2,8 @@ import java.awt.geom.*;     // for Point2D.Double
 import java.util.ArrayList; // for ArrayList
 import gpdraw.*;            // for DrawingTool
 
-public class IrregularPolygon{
+public class IrregularPolygon
+{
     private ArrayList <Point2D.Double> myPolygon;
     private DrawingTool pencil;
 
